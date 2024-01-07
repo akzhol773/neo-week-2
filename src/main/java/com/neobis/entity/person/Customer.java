242ca582db8c1f87187d809c4e2e00b91c3b1e51
akzhol <akzholbek0096@gmail.com>
2024-01-07 15:55:16 +0000

    modified Employee and Customer classes

--- src/main/java/com/neobis/entity/person/Customer.java
+++ src/main/java/com/neobis/entity/person/Customer.java
@@ -1,8 +1,89 @@
 package com.neobis.entity.person;
 
-import java.util.PrimitiveIterator;
+import java.util.Objects;
+/**
+ * Represents a customer, extending the Person class.
+ * This class adds a unique customer ID to the basic personal information inherited from Person.
+ */
 
 public class Customer extends Person {
+
+    // Unique identifier for the customer
     private String customerId;
 
+    /**
+     * Constructor to initialize a Customer object with personal and customer-specific attributes.
+     * This constructor uses the super() method to initialize fields inherited from the Person class.
+     *
+     * @param firstName The first name of the customer.
+     * @param lastName The last name of the customer.
+     * @param contactNumber The contact number of the customer.
+     * @param email The email address of the customer.
+     * @param customerId The unique identifier for the customer.
+     */
+
+    public Customer(String firstName, String lastName, int contactNumber, String email, String customerId) {
+        super(firstName, lastName, contactNumber, email);
+        this.customerId = customerId;
+    }
+
+    /**
+     * Default constructor.
+     * Creates an empty Customer object. Useful when customer details are set using setters.
+     */
+    public Customer() {
+    }
+
+
+    // Getter and Setter methods for customerId
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+
+
+    /**
+     * Determines if this Customer is equal to another object.
+     * Equality is based on both the inherited Person fields and the Customer-specific field (customerId).
+     *
+     * @param o The object to compare with this Customer.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer customer)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(getCustomerId(), customer.getCustomerId());
+    }
+
+
+    /**
+     * Generates a hash code for the Customer.
+     * Includes hash codes from both the inherited Person fields and the Customer-specific field.
+     *
+     * @return A hash code value for the Customer.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getCustomerId());
+    }
+
+    /**
+     * Returns a string representation of the Customer object.
+     * Includes details from both the Person superclass and the Customer-specific field.
+     *
+     * @return A string representation of the Customer object.
+     */
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                "} " + super.toString();
+    }
 }
--- src/main/java/com/neobis/entity/person/Employee.java
+++ src/main/java/com/neobis/entity/person/Employee.java
@@ -1,6 +1,7 @@
 package com.neobis.entity.person;
 
 
+import java.util.Objects;
 
 /**
  * Represents an employee, extending the Person class.
@@ -10,9 +11,89 @@ public class Employee extends Person {
     // The role of the employee (e.g., Manager, Salesperson)
     private String role;
 
-    // The bookstore branch where the employee works
-
+    // Employee ID
     private String employeeId;
 
+    /**
+     * Constructor to initialize an Employee object with personal and employee-specific attributes.
+     * This constructor uses the super() method to initialize fields inherited from the Person class.
+     *
+     * @param firstName The first name of the employee.
+     * @param lastName The last name of the employee.
+     * @param contactNumber The contact number of the employee.
+     * @param email The email address of the employee.
+     * @param role The role of the employee.
+     * @param employeeId The unique identifier for the employee.
+     */
 
+    public Employee(String firstName, String lastName, int contactNumber, String email, String role, String employeeId) {
+        super(firstName, lastName, contactNumber, email);
+        this.role = role;
+        this.employeeId = employeeId;
+    }
+
+  public Employee(){
+
+  }
+
+
+    // Getter and Setter methods for the role and employeeId
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    // Override methods from the Object class
+
+    /**
+     * Determines if this Employee is equal to another object.
+     * Equality is based on both the inherited Person fields and the Employee-specific fields.
+     *
+     * @param o The object to compare with this Employee.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        if (!super.equals(o)) return false; // Check equality based on Person fields
+        return Objects.equals(getRole(), employee.getRole()) &&
+                Objects.equals(getEmployeeId(), employee.getEmployeeId());
+    }
+
+    /**
+     * Generates a hash code for the Employee.
+     * Includes hash codes from both the inherited Person fields and the Employee-specific fields.
+     *
+     * @return A hash code value for the Employee.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getRole(), getEmployeeId());
+    }
+
+    /**
+     * Returns a string representation of the Employee object.
+     * Includes details from both the Person superclass and the Employee-specific fields.
+     *
+     * @return A string representation of the Employee object.
+     */
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "role='" + role + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                "} " + super.toString(); // Includes Person details in the string representation
+    }
 }
