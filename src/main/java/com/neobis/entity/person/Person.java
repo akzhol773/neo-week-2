24644dd89b0c2e89ac552c4660c879e67fafa7c6
akzhol <akzholbek0096@gmail.com>
2024-01-07 15:45:16 +0000

    modified Person class

--- src/main/java/com/neobis/entity/person/Person.java
+++ src/main/java/com/neobis/entity/person/Person.java
@@ -18,4 +18,107 @@ public class Person {
     private String email;
 
 
+    /**
+     * Constructor to initialize a Person object with all its attributes.
+     * This is useful when all person details are known and need to be set at the time of object creation.
+     *
+     * @param firstName The first name of the person.
+     * @param lastName The last name of the person.
+     * @param contactNumber The contact number of the person.
+     * @param email The email address of the person.
+     */
+    public Person(String firstName, String lastName, int contactNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNumber = contactNumber;
+        this.email = email;
+    }
+
+    /**
+     * Default constructor.
+     * Creates an empty Person object. Useful when person details are set using setters.
+     */
+    public Person() {
+    }
+
+
+
+    // Getter and Setter methods for each field
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Override methods from the Object class
+
+    /**
+     * Determines if this Person is equal to another object.
+     * Two Persons are considered equal if their first names, last names, contact numbers, and emails are equal.
+     *
+     * @param o The object to compare with this Person.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return getContactNumber() == person.getContactNumber() &&
+                Objects.equals(getFirstName(), person.getFirstName()) &&
+                Objects.equals(getLastName(), person.getLastName()) &&
+                Objects.equals(getEmail(), person.getEmail());
+    }
+
+    /**
+     * Generates a hash code for the Person.
+     * This is used in collections like HashMap.
+     *
+     * @return A hash code value for the Person.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getContactNumber(), getEmail());
+    }
+
+    /**
+     * Returns a string representation of the Person object.
+     * Includes the person's first name, last name, contact number, and email.
+     *
+     * @return A string representation of the Person object.
+     */
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", contactNumber=" + contactNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
